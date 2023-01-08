e04dd4d1559941122f35688ecf015dd3c5d23637
Dillon <dpapenfu@chicagobooth.edu>
2023-01-08 20:47:40 +0000

    more refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
   # get"/",  controller:  "movies", action: "index"
   root "movies#index"
-  #this is a nice shortcut. i'm not sure if you can do it in other areas
+  #this is a nice shortcut. i'm not sure if you can do it in other areas. it is the way to seet the first... or home page
+
   # Routes for the Movie resource:
 
   # CREATE
-  post"/movies" => "movies#create"
-  get"/movies/new"=> "movies#new"
+  post"/movies" => "movies#create", as: :movies #movies_url and movies_path
+  get"/movies/new"=> "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
   # READ
   get"/movies" => "movies#index"
-  get"/movies/:id" => "movies#show"
+  get"/movies/:id" => "movies#show", as: :movie #movie_path() !!! expects an argument to populate :id segment
   
   # UPDATE
   patch"/movies/:id" => "movies#update"
-  get"/movies/:id/edit"=> "movies#edit"
+  get"/movies/:id/edit"=> "movies#edit", as: :edit_movie #movie_path() !!! expects an argument to populate :id segment
   
   # DELETE
   delete"/movies/:id"=> "movies#destroy"
