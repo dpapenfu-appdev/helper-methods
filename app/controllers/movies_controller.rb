14c7edc4d262a578009e81de91a17a13e41dc448
Dillon <dpapenfu@chicagobooth.edu>
2023-01-08 21:29:39 +0000

    dropped as many render as possible

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,8 +2,7 @@ class MoviesController < ApplicationController
   def new
     @the_movie = Movie.new
 
-    render template: "movies/new.html.erb"
-  end
+   end
 
   def index
     matching_movies = Movie.all
@@ -15,11 +14,11 @@ class MoviesController < ApplicationController
         render json: @list_of_movies
       end
 
-      format.html do
-        render template: "movies/index.html.erb"
+      format.html
+        
       end
-    end
   end
+  
 
   def show
     the_id = params.fetch(:id)
@@ -28,7 +27,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template: "movies/show.html.erb" 
+    
   end
 
   def create
@@ -40,7 +39,7 @@ class MoviesController < ApplicationController
       @the_movie.save
       redirect_to(movies_url { :notice => "Movie created successfully." })
     else
-      render template: "movies/new.html.erb"
+      render template: "new"
     end
   end
 
@@ -51,7 +50,7 @@ class MoviesController < ApplicationController
 
     @the_movie = matching_movies.first
 
-    render template:  "movies/edit.html.erb" 
+    
   end
 
   def update
