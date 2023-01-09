7aa363fff5de6d8600a91332288bc01f629f4803
Dillon <dpapenfu@chicagobooth.edu>
2023-01-09 18:29:20 +0000

    did a lot

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -49,18 +49,20 @@ class MoviesController < ApplicationController
   end
 
   def update
-    # movie_attributes = params.require(:movie).permit(:title, :description)
+    
+    movie_attributes = params.require(:movie).permit(:title, :description)
     # @movie = Movie.new(movie_attributes)
     @movie = Movie.find(params.fetch(:id))
-
-    @movie.title = params.fetch(:title)
-    @movie.description = params.fetch(:description)
+    @movie = Movie.update(movie_attributes)
+    # @movie.title = params.fetch(:movie).fetch(:title)
+    # @movie.description = params.fetch(:movie).fetch(:description)
+   
 
     if @movie.valid?
       @movie.save
-      redirect_to movie_url(the_movie), notice: "Movie updated successfully." 
+      redirect_to movie_url(@movie), notice: "Movie updated successfully." 
     else
-      redirect_to movie_url(the_movie), alert: "Movie failed to update successfully."
+      redirect_to movie_url(@movie), alert: "Movie failed to update successfully."
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
+  
   # get"/",  controller:  "movies", action: "index"
   root "movies#index"
   #this is a nice shortcut. i'm not sure if you can do it in other areas. it is the way to seet the first... or home page
 
   # Routes for the Movie resource:
+  resources :movies
+  resources :directors
 
-  # CREATE
-  post"/movies" => "movies#create", as: :movies #movies_url and movies_path
-  get"/movies/new"=> "movies#new", as: :new_movie #new_movie_url and new_movie_path
+  # # CREATE
+  # post"/movies" => "movies#create", as: :movies #movies_url and movies_path
+  # get"/movies/new"=> "movies#new", as: :new_movie #new_movie_url and new_movie_path
           
-  # READ
-  get"/movies" => "movies#index"
-  get"/movies/:id" => "movies#show", as: :movie #movie_path() !!! expects an argument to populate :id segment
+  # # READ
+  # get"/movies" => "movies#index"
+  # get"/movies/:id" => "movies#show", as: :movie #movie_path() !!! expects an argument to populate :id segment
   
-  # UPDATE
-  patch"/movies/:id" => "movies#update"
-  get"/movies/:id/edit"=> "movies#edit", as: :edit_movie #movie_path() !!! expects an argument to populate :id segment
+  # # UPDATE
+  # patch"/movies/:id" => "movies#update"
+  # get"/movies/:id/edit"=> "movies#edit", as: :edit_movie #movie_path() !!! expects an argument to populate :id segment
   
-  # DELETE
-  delete"/movies/:id"=> "movies#destroy"
+  # # DELETE
+  # delete"/movies/:id"=> "movies#destroy"
 
   #------------------------------
 end
